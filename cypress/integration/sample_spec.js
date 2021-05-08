93ee424502c685a6d28c162aebf9aa1583c70fd4
Rafael Fonseca <rfonseca85@yahoo.ca>
2021-05-08 01:12:26 -0400

    initial commit

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,24 @@
+// describe('My First Test', () => {
+//     it('clicking "type" shows the right headings', () => {
+//         cy.pause()
+//
+//         cy.visit('https://example.cypress.io')
+//
+//         cy.contains('type').click()
+//
+//         // Should be on a new URL which includes '/commands/actions'
+//         cy.url().should('include', '/commands/actions')
+//
+//         // Get an input, type into it and verify that the value has been updated
+//         cy.get('.action-email')
+//             .type('fake@email.com')
+//             .should('have.value', 'fake@email.com')
+//     })
+// })
+
+describe('Check product Amazon', () => {
+  it('clicking "type" shows the right headings', () => {
+    cy.pause();
+    cy.visit('');
+  });
+});
